501e000f7e462b45fd5bf2a296eba82fffbc8a95
Mohammed Ashfaq Hussain <mohammedashfaqhussain@Mohammeds-MacBook-Air.local>
2023-10-28 07:34:09 +0530

    updated with swagger-api docs:

--- accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -20,18 +20,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 				description = "EazyBank Accounts microservice REST API Documentation",
 				version = "v1",
 				contact = @Contact(
-						name = "Madan Reddy",
-						email = "tutor@eazybytes.com",
-						url = "https://www.eazybytes.com"
+						name = "Mohammed Ashfaq",
+						email = "mdashfaqhussain510@gmail.com",
+						url = "https://www.demo.com"
 				),
 				license = @License(
 						name = "Apache 2.0",
-						url = "https://www.eazybytes.com"
+						url = "https://www.demo.com"
 				)
 		),
 		externalDocs = @ExternalDocumentation(
 				description =  "EazyBank Accounts microservice REST API Documentation",
-				url = "https://www.eazybytes.com/swagger-ui.html"
+				url = "https://www.demo.com/swagger-ui.html"
 		)
 )
 public class AccountsApplication {
